79e661ac850765cc62269b60b156fb2f9faf0c8d
Rodrigo Ramírez <yitodeveloper@gmail.com>
2020-09-29 17:19:02 -0300

    cambios en correo de confirmación

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ const AWS_SECRET_ID = "SnFwpz2YIaiHr/E0Q1FB3qGPwkF/SBEsJyUvW+V3";
 const AWS_REGION = "sa-east-1";
 
 const TEST_FROM_EMAIL_ADDRESS = "Centro de Notificaciones <no-reply@getAgil.com>";
-const TEST_TO_EMAIL_ADDRESSES = ['rodrigo@benefy.cl', 'leandro@benefy.cl', 'raul@benefy.cl', 'raulfmir@hotmail.com'];
+const TEST_TO_EMAIL_ADDRESSES = ['rodrigo@benefy.cl', 'leandro@benefy.cl', 'raul@benefy.cl', 'raulfmir@hotmail.com', 'Notafamousman@gmail.com'];
 const TEST_REPLY_TO_EMAIL = ["Soporte Clientes <ayuda@getagil.com>"];
 
 module.exports = {
