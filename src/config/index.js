375367fcbd24016254f50d0e160070ae50358a97
danaemunoz-agil <matias.lara@getagil.com>
2026-04-23 11:09:57 -0400

    Actualizacion Plantilla correo PagoComercio

--- src/config/index.js
+++ src/config/index.js
@@ -2,9 +2,9 @@ const AWS_REGION = process.env.AWS_REGION || "us-east-2";
 
 const TEST_FROM_EMAIL_ADDRESS = "Centro de Notificaciones <no-reply@getAgil.com>";
 const TEST_TO_EMAIL_ADDRESSES = [
-  "carlos.bonomelli@getagil.com",
-  "cristopher.coopman@getagil.com",
-  "ti@getagil.com",
+  "matias.lara@getagil.com",
+  "jose.flores@getagil.com",
+  "raul@getagil.com",
 ];
 const TEST_REPLY_TO_EMAIL = ["Soporte Clientes <ayuda@getagil.com>"];
 
